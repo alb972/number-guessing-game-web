abdfc3ae27cc5594d8c63628c6a4ad74adfd4eb8
Andy DUFRENOT <andy.dufrenot@outlook.com>
2021-08-29 20:09:30 +0200

    [Dev] Manage different environments

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const APPNAME = "popafood-admin-app";
-export const API_URL = "https://api.alby-planet.com/api/v1";
+
+export const API_URL = process.env.API_URL ? process.env.API_URL : "https://api.alby-planet.com/api/v1";
--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,47 +1,52 @@
 import * as webpack from "webpack";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import path from "path";
+import Dotenv from "dotenv-webpack";
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./public/index.html"
+  template: "./public/index.html",
 });
 
 const config: webpack.Configuration = {
-    mode: "production",
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-      rules: [
-        {
-         test: /\.(ts|tsx)$/,
-         loader: "ts-loader",
-         options: {
-            compilerOptions: {
-              "noEmit": false
-            }
-         },
-         exclude: /node_modules/
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        options: {
+          compilerOptions: {
+            noEmit: false,
+          },
         },
-        {
-          test: /\.scss$/,
-          use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-          }, {
-            loader: "css-loader" // translates CSS into CommonJS
-          }, {
-            loader: "sass-loader" // compiles SASS to CSS
-          }]
-        }
-      ]
-    },
-    plugins: [htmlPlugin]
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // compiles SASS to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin, new Dotenv()],
 };
 
 export default config;
