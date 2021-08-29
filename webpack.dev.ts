a16582213436569320524831d882e7311bac47b1
Andy DUFRENOT <andy.dufrenot@outlook.com>
2021-08-29 20:09:30 +0200

    [Dev] Manage different environments

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -2,6 +2,7 @@ import * as webpack from "webpack";
 import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import path from "path";
+import Dotenv from "dotenv-webpack";
 
 // For accept devServer properties
 interface WebPackConfiguration extends webpack.Configuration {
@@ -9,48 +10,52 @@ interface WebPackConfiguration extends webpack.Configuration {
 }
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./public/index.html"
+  template: "./public/index.html",
 });
 
 const config: WebPackConfiguration = {
-    mode: "development",
-    entry: "./src/index.tsx",
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        compress: true,
-        overlay: true,
-        liveReload: true,
-        port: 8080,
-    },
-    module: {
-      rules: [
-        {
-         test: /\.(ts|tsx)$/,
-         loader: "ts-loader",
-         options: {
-            compilerOptions: {
-              "noEmit": false
-            }
-         },
-         exclude: /node_modules/
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    overlay: true,
+    liveReload: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        options: {
+          compilerOptions: {
+            noEmit: false,
+          },
         },
-        {
-          test: /\.scss$/,
-          use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-          }, {
-            loader: "css-loader" // translates CSS into CommonJS
-          }, {
-            loader: "sass-loader" // compiles SASS to CSS
-          }]
-        }
-      ]
-    },
-    plugins: [htmlPlugin]
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // compiles SASS to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin, new Dotenv()],
 };
 
 export default config;
